e10b92e3308a0d62dc01293f620c864248e359aa
buscandoaverroes <56852146+buscandoaverroes@users.noreply.github.com>
2022-12-24 23:40:59 +0900

    add code.py

---
+++ code.py
@@ -0,0 +1,13 @@
+import board
+import digitalio
+import time
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    print("Hello, CircuitPython!")
+    led.value = True
+    time.sleep(1.0)
+    led.value = False
+    time.sleep(1.0)
